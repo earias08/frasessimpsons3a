25306470a4fc9828a979c099fca7947b350d02b5
Emilse <emilsearias@gmail.com>
2021-04-07 16:59:45 -0300

    se agrego el maquetado del componente frase

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
-import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import Button from 'react-bootstrap/Button'
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Button from "react-bootstrap/Button";
+import Frase from "./components/Frase";
 
 function App() {
   return (
-    <section className='container text-center my-5'>
-      <img src={process.env.PUBLIC_URL+ 'logo.png'} alt="Logo de los Simpsons"/>
-        <Button variant="warning">Warning</Button>
+    <section className="container text-center my-5">
+      <article className="d-flex flex-column align-items-center">
+        <img
+          src={process.env.PUBLIC_URL + "logo.png"}
+          alt="Logo de los Simpsons"
+          className='w-75'
+        />
+        <Button variant="warning" className='my-5'>Obtener frase</Button>
+      </article>
+      <Frase></Frase>
     </section>
   );
 }
