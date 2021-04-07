98fa5b47eb9b019cde63af90caec0938a7628df6
Emilse <emilsearias@gmail.com>
2021-04-07 18:08:39 -0300

    se agrego la logica para hacer el request a la api y mostrar los datos

--- src/App.js
+++ src/App.js
@@ -2,8 +2,26 @@ import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Button from "react-bootstrap/Button";
 import Frase from "./components/Frase";
+import {useState, useEffect} from 'react';
 
 function App() {
+  // creo el state
+  const [personaje, setPersonaje] = useState({});
+
+  useEffect(()=>{
+    // logica a ejecutar
+    consultarApi();
+  },[]);
+
+  const consultarApi = async()=>{
+    const respuesta = await fetch('https://thesimpsonsquoteapi.glitch.me/quotes');
+    const resultado = await respuesta.json();
+    console.log(respuesta);
+    console.log(resultado[0]);
+    // guardar objeto dentro del state
+    setPersonaje(resultado[0]);
+  }
+
   return (
     <section className="container text-center my-5">
       <article className="d-flex flex-column align-items-center">
@@ -12,9 +30,9 @@ function App() {
           alt="Logo de los Simpsons"
           className='w-75'
         />
-        <Button variant="warning" className='my-5'>Obtener frase</Button>
+        <Button variant="warning" className='my-5' onClick={()=> consultarApi()}>Obtener frase</Button>
       </article>
-      <Frase></Frase>
+      <Frase personaje={personaje}></Frase>
     </section>
   );
 }
--- src/components/Frase.js
+++ src/components/Frase.js
@@ -1,18 +1,18 @@
 import React from "react";
 import { Card } from "react-bootstrap";
 
-const Frase = () => {
+const Frase = (props) => {
   return (
     <Card>
       <Card.Body>
         <div className="row">
           <div className="col-sm-12 col-md-4">
-            <img src="" alt="" />
+            <img src={props.personaje.image} alt={props.personaje.character}/>
           </div>
           <div className="col-sm-12 col-md-8">
-            <Card.Title>Personaje</Card.Title>
+            <Card.Title>{props.personaje.character}</Card.Title>
             <Card.Text>
-              Frase aleatoria
+              {props.personaje.quote}
             </Card.Text>
           </div>
         </div>
